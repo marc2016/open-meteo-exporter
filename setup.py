948c13122aeb496fe492dd61db42f8e8f789d508
Marc Lammers <marc@lammers.dev>
2024-10-20 10:19:09 +0200

    Fixed path

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(name='open_meteo_exporter',  # Change to your package name
                  description="Open Meteo exporter to influxdb",
-                 version='1.0.0',
+                 version='1.0.1',
                  author='Marc Lammers',
                  url='https://github.com/marc2016/open-meteo-exporter',
                  packages=setuptools.find_packages())
